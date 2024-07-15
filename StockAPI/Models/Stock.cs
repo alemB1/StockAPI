253f68786c0f604fce38fd5bdf5a7d24fddeb03e
Alem Belko <alem.belko@edu.fit.ba>
2024-07-15 15:33:46 +0200

    Add project files.

---
+++ StockAPI/Models/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace StockAPI.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty; // avoid null reference errorss
+        public string CompanyName { get; set; }
+    }
+}
