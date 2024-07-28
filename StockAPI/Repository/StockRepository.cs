db4dcfb03a7ca0e578c6c934ae40e8d09222ce01
Alem Belko <alem.belko@edu.fit.ba>
2024-07-28 21:29:01 +0200

    Added pagination

--- StockAPI/Repository/StockRepository.cs
+++ StockAPI/Repository/StockRepository.cs
@@ -39,8 +39,9 @@ namespace StockAPI.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock> CreateAsync(Stock stockModel)
