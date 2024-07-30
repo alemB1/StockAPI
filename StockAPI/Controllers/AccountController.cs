b694c2e0d7fc8b0e95894f1e25e7f205038f7b4f
Alem Belko <alem.belko@edu.fit.ba>
2024-07-30 11:40:50 +0200

    JWT Login

--- StockAPI/Controllers/AccountController.cs
+++ StockAPI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StockAPI.Dtos.Account;
 using StockAPI.Interfaces;
 using StockAPI.Models;
@@ -12,11 +13,42 @@ namespace StockAPI.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokensService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signinManger;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signinManager)
         {
             _userManager = userManager;
             _tokensService = tokenService;
+            _signinManger = signinManager;
+
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null)
+                return Unauthorized("Invalid username");
+
+
+            // include signin manager
+            var result = await _signinManger.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Username not found/Password incorrect");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokensService.CreateToken(user) // new token for user
+                }
+                ); 
+        }
+
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
--- StockAPI/Controllers/StockController.cs
+++ StockAPI/Controllers/StockController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StockAPI.Data;
 using StockAPI.Dtos.Stock;
@@ -22,6 +23,7 @@ namespace StockAPI.Controllers
         }
 
         [HttpGet]
+        [Authorize]     
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query) 
         {
             if (!ModelState.IsValid)
